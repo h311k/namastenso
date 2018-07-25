52eda2a1e8d059cbd537d260114fde46cdfcc018
Haony Duarte Porto Vieira <haonyduarte@gmail.com>
2018-07-24 21:36:29 -0300

    Revisão da atualização do método de adição e atualização de mailService.

--- src/mail/MailServiceDAO.java
+++ src/mail/MailServiceDAO.java
@@ -8,18 +8,10 @@ public class MailServiceDAO {
 
 	public void addUpdateMailService(int idUsuario, String nome, String host, String porta, String usuario, String senha) {
 		EntityManager manager = FabricaConexao.getFactory().createEntityManager();
-
+		
 		manager.getTransaction().begin();
-		MailService ms = manager.find(MailService.class, idUsuario);
-		manager.getTransaction().commit();
-
-		manager.getTransaction().begin();
-		if (ms != null) {
-			ms = new MailService(idUsuario, nome, host, porta, usuario, senha);
-			manager.merge(ms);
-		} else {
-			manager.persist(ms);
-		}
+		MailService ms = new MailService(idUsuario, nome, host, porta, usuario, senha);
+		manager.merge(ms);
 		manager.getTransaction().commit();
 		manager.close();
 	}
