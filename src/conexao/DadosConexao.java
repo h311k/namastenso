19be00d9b7d0f95aff9a446d7cf0df19706f3a43
Haony Duarte Porto Vieira <haonyduarte@gmail.com>
2018-11-01 16:21:32 -0300

    Remoção de classes desnecessárias.

--- src/conexao/DadosConexao.java
+++
@@ -1,7 +0,0 @@
-package conexao;
-
-public class DadosConexao {
-
-	protected static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=namastenso;user=namastenso;password=1985H311kVieir@;";
-	protected static final String DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-}
--- src/conexao/Queries.java
+++
@@ -1,7 +0,0 @@
-package conexao;
-
-public class Queries {
-
-	public static String AUTENTICA_USUARIO = "select email, senha, ativo, dataInscricao from usuarios where email=? and senha=?";
-	
-}
